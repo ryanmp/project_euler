ed315b3bf9f83df9313388a3f1586a2bdd0f4a33
Ryan Phillips <wordsandcolor@gmail.com>
2014-06-24 18:24:28 -0700

    p8
    I don’t mean to boast - but this solution is pretty slick

---
+++ p8.py
@@ -0,0 +1,13 @@
+import numpy
+def main(n): #Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+	f = list(open('p8.in').read().replace(" ", "")) #file -> list
+	f = [int(x) for x in f] #chars -> ints
+	biggest_prod = 0
+	for i in xrange(len(f)-n):
+		prod = numpy.product(f[i:i+n])
+		biggest_prod = max(prod, biggest_prod)
+	return biggest_prod
+
+if __name__ == '__main__':
+	import boilerplate
+	boilerplate.all(main(13)) 
