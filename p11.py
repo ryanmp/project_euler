a91835bced9f58c0f031e5f786f359494d99eb36
Ryan Phillips <wordsandcolor@gmail.com>
2014-06-26 11:21:28 -0700

    searching p-triples and number crossword solving

---
+++ p11.py
@@ -0,0 +1,31 @@
+import numpy
+def main(n): # greatest product of 4 adjacent numbers (8 diff directions including diagonal)
+	lines = open('p11.in').read().split('\n') #file -> list
+	arr = []
+	for i in lines:
+		temp = i.split(' ')
+		arr.append( [int(x) for x in temp] )
+
+	biggest_prod = 0
+	for i in xrange(16): # might need to go to 20 with one of these... 
+		for j in xrange(16): #(i,j) is starting idx
+
+			combos = []
+			combos.append( [arr[i+x][j] for x in xrange(4)] )#down
+			combos.append( [arr[i][j+x] for x in xrange(4)] )#right
+
+			if i < 16:
+				combos.append( [arr[i+x][j+x] for x in xrange(4)] )#diag_DR
+
+			if i > 4:
+				combos.append( [arr[i-x][j+x] for x in xrange(4)] )#diag_DL
+
+			for n in combos:
+				prod = numpy.product(n)
+				biggest_prod = max(prod, biggest_prod)
+
+	return 'biggest_prod', biggest_prod
+
+if __name__ == '__main__':
+	import boilerplate
+	boilerplate.all(main(13)) 
---
+++ p9.py
@@ -0,0 +1,15 @@
+def main(): #return product (a*b*c) of pythagorean triple (s.t. a + b + c = 1000)
+
+	for a in xrange(1,332):
+		for b in xrange(a,499):
+			c = 1000-a-b
+			if a<b and b<c and a+b+c==1000:
+				if is_pyth_triple(a,b,c):
+					return a,b,c,'product:',a*b*c
+
+def is_pyth_triple(a,b,c):
+	return (a*a + b*b == c*c)	
+
+if __name__ == '__main__':
+	import boilerplate
+	boilerplate.all(main())
