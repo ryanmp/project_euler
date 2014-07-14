0083c415541bdb4b48243fc85c9369c5343de738
Ryan Phillips <wordsandcolor@gmail.com>
2014-07-14 12:00:59 -0700

    speed improvement

--- p12.py
+++ p12.py
@@ -1,14 +1,17 @@
+import math
 def main(n): # First triangle number to have over five hundred divisors?
-	i = 0; x = 0
+	i = 0; x = 0 # lower bound....
+	max_divisors = -1
 	while (True):
 		x += 1; i += x
 		how_many_divisors = 0
-		for j in xrange(1,i+1):
+		for j in xrange(1,int(math.sqrt(i))):
 			if i%j==0: #is a divisor
-				how_many_divisors += 1
+				how_many_divisors += 2 # always in pairs
+		max_divisors = max(max_divisors,how_many_divisors)
 		if how_many_divisors > n:
 			return i
 
 if __name__ == '__main__':
 	import boilerplate, time
-	boilerplate.all(time.time(),main(150)) #this solutions is too slow for n = 500 !
+	boilerplate.all(time.time(),main(500)) # under 10 seconds... good enough
