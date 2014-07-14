545dc8579618ef7a467935424986ffc5a73b0749
Ryan Phillips <wordsandcolor@gmail.com>
2014-07-14 11:34:11 -0700

    now meta.py can run all solution scripts

--- meta.py
+++ meta.py
@@ -1,16 +1,30 @@
-# this will
-# analyze number of lines of all p_ and relative running times
+import re, os
+def run_all():
+	all_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+	all_problems = []
+	for i in all_files:
+		m = re.match("^p.*\d.py", i)
+		if m:
+			all_problems.append(i)
 
+	all_problems = all_problems[0:3] # just do a couple for testing purposes
+
+	# runs every solution script in this directory
+	for i in all_problems:
+		print i
+		execfile(i, globals())
+
+# prints the number of lines in every p###.py file in this dir
+def all_lines(files):
+	for i in files:
+		print num_lines(i)
+
+# this will analyze number of lines of all p_ and relative running times
 def num_lines(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1 - (4) #4 for our boilerplate
 
-# prints the number of lines in every p13434.py file in this dir
-import re, os
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for i in files:
-	m = re.match("^p.*\d[.py]", i)
-	if m:
-		print num_lines(i)
+if __name__ == '__main__':
+	run_all()
