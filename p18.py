4c292d5859a7cd141d4c58da4f2f0faf3dd797f0
Ryan Phillips <wordsandcolor@gmail.com>
2014-07-20 11:57:50 -0700

    got it!
    so I ended up going with recursion… if we have a larger input set, a
    little bit of branch pruning will be essential, but it’s not needed for
    this problem

--- p18.py
+++ p18.py
@@ -1,16 +1,28 @@
 def main(): # maximum path-sum from top to bottom
+
+	# input to array ( 'p18_test.in' for a smaller test set )
 	lines = open('p18.in').read().split('\n') #file -> list
 	arr = []
 	for i in lines:
 		temp = i.split(' ')
 		arr.append( [int(x) for x in temp] )
 
-	sum = 0
-	for line in arr:
-		sum += line[0]
+	# recursively sum through all possible paths, keeping the largest sum
+	def add_next(my_sum,arr,row,idx,biggest_sum):
+		my_sum += arr[row][idx]
+		row += 1
+		if row<len(arr):
+			if (idx - 1 >= 0):
+				biggest_sum = add_next(my_sum,arr,row,idx-1,biggest_sum) # idx-1
+			if (idx + 1 <= len(arr[row])):
+				biggest_sum = add_next(my_sum,arr,row,idx+1,biggest_sum) # idx+1
+			biggest_sum = add_next(my_sum,arr,row,idx,biggest_sum) # same idx
 
-	return 'not finished yet!'
+		biggest_sum = max(my_sum, biggest_sum)
+		return biggest_sum
+
+	return add_next(0,arr,0,0,0)
 
 if __name__ == '__main__':
 	import boilerplate, time
-	boilerplate.all(time.time(),main())
+	boilerplate.all(time.time(),main()) # about 1 second... not too bad
